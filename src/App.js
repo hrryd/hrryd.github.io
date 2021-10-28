64045dc725b5eed82e888e11a7ef5803dbc65bb8
Harry Davis <ib.harrydavis@gmail.com>
2021-10-28 13:55:22 +0100

    Basic rendering loop

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef, useEffect } from 'react';
 
 function App() {
+
+  const canvasRef = useRef(null);
+
+  let gameState = {
+    player: {
+      x: 300,
+      y: 300,
+    }
+  };
+
+  let pressedKeys = {};
+  window.onkeyup = function(e) { pressedKeys[e.keyCode] = false; };
+  window.onkeydown = function(e) { pressedKeys[e.keyCode] = true; };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    setInterval(() => {
+      context.fillStyle = '#ffffff';
+      context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+
+      context.beginPath();
+      context.arc(gameState.player.x, gameState.player.y, 25, 0, 2 * Math.PI);
+      context.fillStyle = 'black';
+      context.fill();
+
+      const speed = 5;
+      if (pressedKeys[87]) {
+        gameState.player.y -= speed;
+      }
+      if (pressedKeys[65]) {
+        gameState.player.x -= speed;
+      }
+      if (pressedKeys[83]) {
+        gameState.player.y += speed;
+      }
+      if (pressedKeys[68]) {
+        gameState.player.x += speed;
+      }
+    }, 33);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Test 123
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <canvas ref={canvasRef} id="myCanvas" width="600" height="600" style={{border:"1px solid #000000"}} />
       </header>
     </div>
   );
